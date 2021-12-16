84bf48fe557b58388022117b703696efd395f944
Javoopls <ja.vivancoa@duocuc.cl>
2021-12-16 02:03:20 -0300

    correo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -12,8 +13,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot({}),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    EmailComposer,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/viajes/pedir/detalle-viaje/detalle-viaje.page.ts
+++ src/app/viajes/pedir/detalle-viaje/detalle-viaje.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AlertController, ModalController, NavController } from '@ionic/angular';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import {
+  AlertController,
+  ModalController,
+  NavController,
+} from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { MapModalComponent } from 'src/app/shared/map-modal/map-modal.component';
 import { Viaje } from '../../viaje.model';
@@ -14,6 +19,7 @@ import { ViajesService } from '../../viajes.service';
 export class DetalleViajePage implements OnInit, OnDestroy {
   viaje: Viaje;
   isLoading = false;
+  public correo: 'ja.vivancoa@duocuc.cl';
   private viajeSub: Subscription;
 
   constructor(
@@ -22,11 +28,12 @@ export class DetalleViajePage implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private alertCtrl: AlertController,
     private router: Router,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private emailComposer: EmailComposer
   ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('viajeId')) {
         this.navCtrl.navigateBack('/viajes/tabs/pedir');
         return;
@@ -34,41 +41,74 @@ export class DetalleViajePage implements OnInit, OnDestroy {
       this.isLoading = true;
       this.viajeSub = this.viajesService
         .getViaje(paramMap.get('viajeId'))
-        .subscribe(viaje => {
-          this.viaje = viaje;
-          this.isLoading = false;
-        },
-        (error) => {
-          this.alertCtrl
-            .create({
-              header: 'Un error ha ocurrido!',
-              message: 'No se pudo cargar el viaje',
-              buttons: [
-                {
-                  text: 'Ok',
-                  handler: () => {
-                    this.router.navigate(['/viajes/tabs/pedir']);
+        .subscribe(
+          (viaje) => {
+            this.viaje = viaje;
+            this.isLoading = false;
+          },
+          (error) => {
+            this.alertCtrl
+              .create({
+                header: 'Un error ha ocurrido!',
+                message: 'No se pudo cargar el viaje',
+                buttons: [
+                  {
+                    text: 'Ok',
+                    handler: () => {
+                      this.router.navigate(['/viajes/tabs/pedir']);
+                    },
                   },
-                },
-              ],
-            })
-            .then((alertEl) => alertEl.present());
-        });
+                ],
+              })
+              .then((alertEl) => alertEl.present());
+          }
+        );
     });
   }
 
   mostrarMapa() {
-    this.modalCtrl.create({component: MapModalComponent, componentProps: {
-      center: {lat: this.viaje.lugarViaje.lat, lng: this.viaje.lugarViaje.lng },
-      selectable: false,
-      closeButtonText: 'Cerrar',
-      title: this.viaje.lugarViaje.direccion
-    } }).then(modalEl => {
-      modalEl.present();
-    });
+    this.modalCtrl
+      .create({
+        component: MapModalComponent,
+        componentProps: {
+          center: {
+            lat: this.viaje.lugarViaje.lat,
+            lng: this.viaje.lugarViaje.lng,
+          },
+          selectable: false,
+          closeButtonText: 'Cerrar',
+          title: this.viaje.lugarViaje.direccion,
+        },
+      })
+      .then((modalEl) => {
+        modalEl.present();
+      });
   }
 
-  viajePedido() {
+  enviarCorreo() {
+    const email = {
+      to: this.correo,
+      subject: 'Viaje Solicitado',
+      body:
+        this.viaje.lugarViaje +
+        ' ' +
+        this.viaje.costo +
+        ' ' +
+        this.viaje.horaSalida +
+        ' ' +
+        this.viaje.nombreConductor +
+        ' ' +
+        this.viaje.patenteVehiculo,
+    };
+    this.emailComposer.open(email);
+    console.log(email);
+  }
+
+  viajePedido(userId) {
+    this.viajesService.getViaje(userId);
+    this.enviarCorreo();
+    console.log(userId);
+    // this.viajesService.crearViaje(lugarViaje, costo, horaSalida);
     this.navCtrl.navigateBack('/viajes/tabs/pedir');
   }
 
